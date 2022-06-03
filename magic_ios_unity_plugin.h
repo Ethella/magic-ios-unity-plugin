afbea2a3fc3a0a04a99cd8f50b64e56c682fea68
ethella <jerry@magic.link>
2022-06-03 16:54:45 -0700

    * add framework workspace

---
+++ magic_ios_unity_plugin.h
@@ -0,0 +1,18 @@
+//
+//  magic_ios_unity_plugin.h
+//  magic-ios-unity-plugin
+//
+//  Created by Wentao Liu on 6/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for magic_ios_unity_plugin.
+FOUNDATION_EXPORT double magic_ios_unity_pluginVersionNumber;
+
+//! Project version string for magic_ios_unity_plugin.
+FOUNDATION_EXPORT const unsigned char magic_ios_unity_pluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <magic_ios_unity_plugin/PublicHeader.h>
+
+
