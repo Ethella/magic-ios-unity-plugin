0b11c719b8ca37d04336b2ca4ac3345b769be09d
ethella <jerry@magic.link>
2022-06-03 20:27:17 -0700

    * reformat file structure

---
+++ Sources/Magic-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Magic-Bridging-Header.h
+//  magic-ios-unity-plugin
+//
+//  Created by Wentao Liu on 6/3/22.
+//
+
+#ifndef Magic_Bridging_Header_h
+#define Magic_Bridging_Header_h
+
+
+#endif /* Magic_Bridging_Header_h */
